07b824ad8c64a8f9398e4cc1d900768ff808ef04
Aziz <113197790+haquema@users.noreply.github.com>
2023-05-16 23:01:45 +0100

    update seeds with new format of dates_unavailable field in properties table

--- seeds.sql
+++ seeds.sql
@@ -3,7 +3,7 @@ TRUNCATE TABLE users, properties, bookings RESTART IDENTITY;
 INSERT INTO users (name, email_address, phone, password) VALUES ('azizul haque', 'aziz@gmail.com', 1234567890, '$2a$12$mMb4WtpD.RDFlsmrnx5p8.GjJgloO12VxUuyc6L/ZxIY65Bw1HohO');
 INSERT INTO users (name, email_address, phone, password) VALUES ('sameeul haque', 'samee@gmail.com', 9876543210, '$2a$12$mMb4WtpD.RDFlsmrnx5p8.GjJgloO12VxUuyc6L/ZxIY65Bw1HohO');
 
-INSERT INTO properties (name, description, price, to_rent, date_unavailable, user_id) VALUES ('Modern City Apartment', 'You will be sure to have an out of this world experience in our UFO-styled treehouse', 500, true, '2023-05-252023-05-29 2023-05-302023-05-31', 1);
+INSERT INTO properties (name, description, price, to_rent, date_unavailable, user_id) VALUES ('Modern City Apartment', 'You will be sure to have an out of this world experience in our UFO-styled treehouse', 500, true, '2023-05-25,2023-05-26,2023-05-27,2023-05-28,2023-05-29,2023-05-30,2023-05-31', 1);
 INSERT INTO properties (name, description, price, to_rent, date_unavailable, user_id) VALUES ('Beachside Condo', 'sun, sea and the cool breeze - what more could you ask for?', 400, true, '', 2);
 INSERT INTO properties (name, description, price, to_rent, date_unavailable, user_id) VALUES ('Cottage', 'escape the busy city and enjoy the idyllic countrysides of southern England', 200, true, '', 1);
 INSERT INTO properties (name, description, price, to_rent, date_unavailable, user_id) VALUES ('Family Home', 'Perfect first time home for young couples looking to grow their family', 250, true, '', 2);
@@ -12,3 +12,5 @@ INSERT INTO properties (name, description, price, to_rent, date_unavailable, use
 INSERT INTO bookings (property_id, booker_id, start_date, end_date, status) VALUES (1, 2, '2023-05-25', '2023-05-29', 'confirmed');
 INSERT INTO bookings (property_id, booker_id, start_date, end_date, status) VALUES (1, 2, '2023-05-30', '2023-05-31', 'confirmed');
 INSERT INTO bookings (property_id, booker_id, start_date, end_date, status) VALUES (3, 1, '2023-05-29', '2023-06-06', 'pending');
+
+2023-05-25,2023-05-26,2023-05-27,2023-05-28,2023-05-29,2023-05-30,2023-05-31
